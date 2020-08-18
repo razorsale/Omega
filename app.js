4a1bc4ad8204610d7186d19e74c4cf86311d9de7
razorsale <69877140+razorsale@users.noreply.github.com>
2020-08-19 07:25:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDNi9-ldVW6MVfliv7lToYBTk_zWl2Fmxg",
+  authDomain: "razorforsale-78284.firebaseapp.com",
+  databaseURL: "https://razorforsale-78284.firebaseio.com",
+  projectId: "razorforsale-78284",
+  storageBucket: "razorforsale-78284.appspot.com",
+  messagingSenderId: "253261679513",
+  appId: "1:253261679513:web:b259edf40bb28f57a4e108",
+  measurementId: "G-5CJLN123G9"
 };
 
      
